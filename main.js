e32b2d5bc5e0d9e69f12b1c06558a93181c8ae43
tomatopaster <alanthan4242@gmail.com>
2025-09-15 17:34:03 -0700

    Testing rendering

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 import * as THREE from 'three';
 
+const scene = new THREE.Scene();
+const FOV = 75;
+const AspectRatio = window.innerWidth / window.innerHeight;
+const NearLimit = .1;
+const FarLimit = 1000;
+const camera = new THREE.PerspectiveCamera( FOV, AspectRatio, NearLimit, FarLimit );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
