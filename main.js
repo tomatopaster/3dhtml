6a0f6ee2c798aa92dc6189cf378eab1fa33514af
tomatopaster <alanthan4242@gmail.com>
2025-09-16 17:13:11 -0700

    Seeing lines

--- main.js
+++ main.js
@@ -11,12 +11,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.LineBasicMaterial( { color: 0xFFFFFF } );
+const points = [];
+points.push( new THREE.Vector3( 1, 0, 0 ) );
+points.push( new THREE.Vector3( 1, 2, 0 ) );
+points.push( new THREE.Vector3( 0, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
 
 camera.position.z = 5;
+
+scene.add(line);
 
 function animate() {
   renderer.render( scene, camera );
