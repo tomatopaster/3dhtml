c23860b4b38a46798b4ed6cfd8dfe15c588be565
tomatopaster <alanthan4242@gmail.com>
2025-09-16 19:51:52 -0700

    Testing Line2

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 
+import { Line2 } from 'three/examples/jsm/lines/Line2.js';
+import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js';
+import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
+
 const scene = new THREE.Scene();
 const FOV = 75;
 const AspectRatio = window.innerWidth / window.innerHeight;
@@ -11,7 +15,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const material = new THREE.LineBasicMaterial( { color: 0xFFFFFF } );
+const materialTwo = new LineMaterial( { color: 0xFFFFFF, linewidth: 3 } );
+
+const pointsTwo = [];
+pointsTwo.push( new THREE.Vector3( -1, 0, 0 ) );
+pointsTwo.push( new THREE.Vector3( -1, -2, 0 ) );
+pointsTwo.push( new THREE.Vector3( 0, 0, 0 ) );
+
+const geometryTwo = new LineGeometry().setFromPoints( pointsTwo );
+
+const lineTwo = new Line2( geometryTwo, materialTwo ) ;
+
+// ------------------------------------------------------------------------------
+
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
 const points = [];
 points.push( new THREE.Vector3( 1, 0, 0 ) );
 points.push( new THREE.Vector3( 1, 2, 0 ) );
@@ -24,6 +42,7 @@ const line = new THREE.Line( geometry, material );
 camera.position.z = 5;
 
 scene.add(line);
+scene.add(lineTwo);
 
 function animate() {
   renderer.render( scene, camera );
